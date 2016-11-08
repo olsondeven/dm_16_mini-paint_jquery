6dabb7f5d7630c8c4477772d6c9dc0126e87888b
olsondeven <olsondeven@gmail.com>
2016-11-08 15:56:43 -0700

    finished

---
+++ app.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+  var color = 'white';
+  var colors = 'white green red blue yellow';
+
+  $('#red').on('click', function() {
+    color = 'red';
+  })
+
+  $('#blue').on('click', function() {
+    color = 'blue';
+  })
+
+  $('#green').on('click', function() {
+    color = 'green';
+  })
+
+  $('#yellow').on('click', function() {
+    color = 'yellow';
+  })
+
+  $('#white').on('click', function() {
+    color = 'white';
+  })
+//affects that class that they
+  $('.box').on('hover', function(){
+    $(this).addClass(color);
+  })
+
+  $('.box').on('dblclick',function(){
+    $(this).removeClass(colors);
+  })
+
+  $('#reset').on('click', function() {
+    $('.box').removeClass(colors)
+  })
+
+});//closing
